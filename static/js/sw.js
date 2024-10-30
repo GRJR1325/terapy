a739eff9d17d084f14dcca65bcc1333dc50331b8
José Rolando <97265804+GRJR1325@users.noreply.github.com>
2024-10-30 16:54:56 -0600

    Update service worker.js

--- static/js/sw.js
+++ static/js/sw.js
@@ -1,14 +1,49 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+    '/index.html',
+    '/static/css/styles.css',
+    '/static/css/materialize.min.css',
+    '/static/css/materialize.css',
+    '/static/js/materialize.min.js',
+    '/static/js/materialize.js',
+    '/static/js/script.js',
+    '/static/images/loge2.webp',
+    '/manifest.json'
+];
+
+// Evento de instalación
 self.addEventListener('install', function(event) {
     event.waitUntil(
-      caches.open('pwa-cache').then(function(cache) {
-        return cache.addAll([
-          '/index.html',
-          '/static/css/styles.css',
-          '/static/js/script.js',
-          '/static/images/loge2.webp',
-          '/manifest.json'
-        ]);
-      })
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+            .then(self.skipWaiting())
     );
-  });
-  
+});
+
+// Evento de activación (para limpiar cachés antiguas)
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Evento de fetch (manejo de solicitudes de red)
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
